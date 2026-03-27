f5e68b7a87dd52255314eac8d4b23c2e31f380a0
nemerald-voip <58834187+nemerald-voip@users.noreply.github.com>
2026-03-27 01:00:41 -0700

    updated config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
     },
 
     // Set the production url of your site here
-    url: 'https://your-docusaurus-site.example.com',
+    url: 'https://nemerald-voip.github.io',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    baseUrl: '/nemerald-web/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
+    organizationName: 'nemerald-voip', // Usually your GitHub org/user name.
+    projectName: 'nemerald-web', // Usually your repo name.
+    trailingSlash: false, 
 
     onBrokenLinks: 'throw',
 
